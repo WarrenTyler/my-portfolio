19a97145368f377bbe4460307c407b5e4583b32c
WarrenT <warrentyler1024@gmail.com>
2023-11-25 03:17:17 +0000

    Integrate Header component into App with main container and initial styling: - Wrap the entire application in a dark mode container

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-
+import Header from "./components/Header";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
-export default App
+  return (
+    <div className="dark">
+      {/* Main Container (Simulating Body) */}
+      <div className="min-h-screen font-poppins pb-12 text-black dark:text-white bg-body dark:bg-body-dark">
+        <Header />
+      </div>
+    </div>
+  );
+};
+export default App;
