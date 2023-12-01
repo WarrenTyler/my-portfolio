8bd61cc2941357774be816adca9a4016cb0435a2
WarrenT <warrentyler1024@gmail.com>
2023-12-01 04:42:23 +0000

    Improve Projects component with dynamic column span
    - Introduce state and useEffect to dynamically calculate colSpan based on window width
    - Simplify the structure and make it more responsive
    - Update the rendering of ProjectCard components with dynamic col-span classes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,83 +1,64 @@
+import { useEffect, useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { projects } from "../data/projectsData";
 
 const Projects = () => {
+  const [colSpan, setColSpan] = useState(1);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+
+      // Determine colSpan based on the screen size
+      if (windowWidth >= 768 && windowWidth < 1280) {
+        setColSpan(2);
+      } else if (windowWidth >= 1280) {
+        setColSpan(3);
+      } else {
+        setColSpan(1);
+      }
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Attach the event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Function to calculate the col-span class for a ProjectCard based on its position in the array
+  const calculateColSpan = (colSpan, projectsLength, itemIndex) => {
+    // Calculate the number of remaining columns after evenly distributing projects
+    const remainingColumns = projectsLength % colSpan;
+
+    // Check if there are remaining columns and if the current item is within the range of remaining columns
+    // If true, return the col-span class, otherwise, return an empty string
+    return remainingColumns > 0 && itemIndex < remainingColumns
+      ? `col-span-${colSpan}`
+      : "";
+  };
+
   return (
     <div className="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
       <section className="w-full">
         <h2 id="projects" className="secondary-title">
           My projects
         </h2>
-        {/* <h2
-          id="projects"
-          className="before:content-[''] before:block before:w-12 before:h-2 before:bg-theme before:rounded-full before:mb-1 text-3xl font-bold"
-        >
-          My projects
-        </h2> */}
         <p className="section-paragraph">
           I've had the privilege of crafting and implementing a wide range of
           projects, seamlessly blending front-end and back-end technologies.
         </p>
-
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-          <img
-            className="w-full bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-          />
-          <img
-            className="w-full bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1545235617-9465d2a55698?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-          />
-          <img
-            className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1586717791821-3f44a563fa4c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
-          />
-          <img
-            className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1558655146-d09347e92766?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80"
-          />
-          <img
-            className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1547119957-637f8679db1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-          />
-          <img
-            className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1559028012-481c04fa702d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1336&q=80"
-          />
-          <img
-            className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1603969072881-b0fc7f3d77d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80s"
-          />
-          <img
-            className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-          />
-          <img
-            className="w-full hidden md:block md:col-span-2 lg:col-span-1 bg-nav h-36 lg:h-72 object-cover"
-            src="https://images.unsplash.com/photo-1545235617-7a424c1a60cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80"
-          />
-        </div> */}
+        {/* Dynamic grid content */}
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-6">
-          {/* <ProjectCard
-            image="https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-            title="Meme Generator"
-            description="React JS"
-          />
-          <ProjectCard image="" title="" />
-          <ProjectCard image="" title="" />
-          <ProjectCard image="" title="" />
-          <ProjectCard image="" title="" />
-          <ProjectCard image="" title="" />
-          <ProjectCard image="" title="" />
-          <ProjectCard image="" title="" />
-          <ProjectCard
-            image=""
-            title=""
-            spanClasses="md:col-span-2 lg:col-span-1"
-          /> */}
-
-          {projects.map((project) => (
-            <ProjectCard key={project.id} {...project} />
+          {projects.map((project, index) => (
+            <ProjectCard
+              spanClass={calculateColSpan(colSpan, projects.length, index)}
+              key={project.id}
+              {...project}
+            />
           ))}
         </div>
       </section>
