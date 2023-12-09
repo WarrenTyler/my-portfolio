9625d2237cead4ddb84e49d5ae78eeacc7b49f01
WarrenT <warrentyler1024@gmail.com>
2023-12-09 00:11:04 +0000

    Add project images and update projects data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,8 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 export const projects = [
   {
     id: uuidv4(),
-    image:
-      "https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    image: "./src/images/meme-generator-square.png",
     title: "Meme Generator",
     description: "React JS",
     view: "",
@@ -12,8 +11,7 @@ export const projects = [
   },
   {
     id: uuidv4(),
-    image:
-      "https://images.unsplash.com/photo-1545235617-9465d2a55698?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    image: "./src/images/aivisio3.jpg",
     title: "AiVisio",
     description: "A fullstack AI image generation tool",
     view: "",
@@ -21,8 +19,16 @@ export const projects = [
   },
   {
     id: uuidv4(),
-    image:
-      "https://images.unsplash.com/photo-1586717791821-3f44a563fa4c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    image: "./src/images/weather-dashboard.jpg",
+    title: "Weather Dashboard",
+    description:
+      "A JavaScript web application that uses a 5 Day Weather Forecast API to retrieve and store weather data for cities.",
+    view: "https://warrentyler.github.io/work-day-scheduler/",
+    github: "https://github.com/WarrenTyler/work-day-scheduler",
+  },
+  {
+    id: uuidv4(),
+    image: "./src/images/weather-dashboard.jpg",
     title: "Work Day Scheduler",
     description:
       "A JavaScript web application that allows users to save events for 9:00-17:00 work hours of the day.",
