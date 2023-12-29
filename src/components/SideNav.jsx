cedadd019dbcf3948df4c7b05ad06f608bb3c263
WarrenT <warrentyler1024@gmail.com>
2023-12-29 13:34:08 +0000

    Create SideNav component
    - Extract SideNav component from Hero component
    - Utilize useScroll hook from ScrollContext for active section tracking
    - Implement navigation links with dynamic styling based on active section
    - Add tooltips for improved accessibility
    - Enhance styling and positioning for a clean and responsive design

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,73 @@
+import { useScroll } from "../context/ScrollContext";
+
+const SideNav = () => {
+  const { activeSection, setSection } = useScroll();
+
+  const handleClick = (sectionId) => {
+    setSection(sectionId);
+  };
+
+  return (
+    <nav className="z-10 hidden lg:block lg:w-8 fixed top-[20vh] left-[5vw]">
+      <div className="absolute space-y-6 mt-36">
+        <a
+          href="#"
+          className={`side-nav__link ${
+            activeSection === "home"
+              ? "side-nav__link--selected"
+              : "side-nav__link--not-selected"
+          }`}
+          onClick={() => handleClick("home")}
+        >
+          <span className="side-nav__tooltip">Home</span>
+        </a>
+        <a
+          href="#about"
+          className={`side-nav__link ${
+            activeSection === "about"
+              ? "side-nav__link--selected"
+              : "side-nav__link--not-selected"
+          }`}
+          onClick={() => handleClick("about")}
+        >
+          <span className="side-nav__tooltip">About</span>
+        </a>
+        <a
+          href="#projects"
+          className={`side-nav__link ${
+            activeSection === "projects"
+              ? "side-nav__link--selected"
+              : "side-nav__link--not-selected"
+          }`}
+          onClick={() => handleClick("projects")}
+        >
+          <span className="side-nav__tooltip">Projects</span>
+        </a>
+        <a
+          href="#skills"
+          className={`side-nav__link ${
+            activeSection === "skills"
+              ? "side-nav__link--selected"
+              : "side-nav__link--not-selected"
+          }`}
+          onClick={() => handleClick("skills")}
+        >
+          <span className="side-nav__tooltip">Skills</span>
+        </a>
+        <a
+          href="#hire"
+          className={`side-nav__link ${
+            activeSection === "hire"
+              ? "side-nav__link--selected"
+              : "side-nav__link--not-selected"
+          }`}
+          onClick={() => handleClick("hire")}
+        >
+          <span className="side-nav__tooltip">Hire</span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default SideNav;
