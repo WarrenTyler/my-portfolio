b3c40bd0855ab8fc04a23679efde1ee60e204da3
WarrenT <warrentyler1024@gmail.com>
2023-12-30 10:39:14 +0000

    Refactor Skills component
    - Update section structure for consistency with section__layout class
    - Reorganize HTML and CSS classes for improved readability
    - Use consistent naming conventions for skill elements

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,52 +11,49 @@ import {
 
 const Skills = () => {
   return (
-    <section id="skills">
-      <div className="w-[80%] mx-auto mt-24">
-        <h2 id="skills" className="secondary-title">
-          My Skills
+    <div className="section__layout">
+      <section className="w-full">
+        <h2 id="skills" className="section__title">
+          My skills
         </h2>
-        <div id="skills-list">
           <div
-            id="skills-highlight"
             className="flex flex-wrap justify-around text-center"
           >
-            <div className="box">
-              <BiLogoHtml5 size="8rem" className="text-black" />
+            <div className="skill__div">
+              <BiLogoHtml5 className="skill__logo" />
               <h3>HTML 5</h3>
             </div>
-            <div className="box">
-              <BiLogoCss3 size="8rem" />
+            <div className="skill__div">
+              <BiLogoCss3 className="skill__logo" />
               <h3>CSS 3</h3>
             </div>
-            <div className="box">
-              <BiLogoJavascript size="8rem" />
+            <div className="skill__div">
+              <BiLogoJavascript className="skill__logo" />
               <h3>Javascript</h3>
             </div>
-            <div className="box">
-              <BiLogoReact size="8rem" />
+            <div className="skill__div">
+              <BiLogoReact className="skill__logo" />
               <h3>React JS</h3>
             </div>
-            <div className="box">
-              <BiLogoTailwindCss size="8rem" />
+            <div className="skill__div">
+              <BiLogoTailwindCss className="skill__logo" />
               <h3>Tailwind CSS</h3>
             </div>
-            <div className="box">
-              <BiLogoMongodb size="8rem" />
+            <div className="skill__div">
+              <BiLogoMongodb className="skill__logo" />
               <h3>MongoDB</h3>
             </div>
-            <div className="box">
-              <BiLogoFigma size="8rem" />
+            <div className="skill__div">
+              <BiLogoFigma className="skill__logo" />
               <h3>Figma</h3>
             </div>
-            <div className="box">
-              <BiLogoGithub size="8rem" />
+            <div className="skill__div">
+              <BiLogoGithub className="skill__logo" />
               <h3>GitHub</h3>
             </div>
           </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 export default Skills;
