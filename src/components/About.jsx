50f785a5939339590dfd833646d233e25b8ff1d7
WarrenT <warrentyler1024@gmail.com>
2023-12-30 10:30:39 +0000

    Refactor About component
    - Apply consistent naming conventions for CSS classes (section__layout, section__title, section__paragraph)
    - Improve readability and maintainability with updated class names
    - Enhance structure for better styling and future modifications
    - Maintain functionality for toggling additional content with "Read more" button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
-const AboutMe = () => {
+const About = () => {
   const [showMore, setShowMore] = useState(false);
 
   return (
-    <section className="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+    <section className="section__layout">
       <section className="w-full">
-        <h2 id="clients" className="secondary-title">
+        <h2 id="about" className="section__title">
           About
         </h2>
-        <p className="section-paragraph">
+        <p className="section__paragraph">
           Greetings! I&lsquo;m Warren, an aspiring Front-end web developer eager
           to bring my skills and creativity to the world of web development. My
           journey into this exciting field began with a three-month bootcamp,
           where I immersed myself in HTML, CSS, Tailwind, JavaScript, React, and
           the intricacies of version control through GitHub.
         </p>
-        <p className="section-paragraph">
+        <p className="section__paragraph">
           I possess a solid foundation in HTML, CSS, JavaScript, React, and
           Figma, along with proficiency in MongoDB. My background in Computer
           Science, including AI, Java, SQL database design, Software Engineering
@@ -26,21 +26,21 @@ const AboutMe = () => {
         </p>
         {showMore && (
           <>
-            <p className="section-paragraph">
+            <p className="section__paragraph">
               I approach front-end development as a creative endeavor,
               seamlessly merging technology and artistry to craft visually
               stunning and user-friendly experiences. My passion lies in
               bridging the digital and tangible worlds, breathing life into
               interfaces that engage and inspire.
             </p>
-            <p className="section-paragraph">
+            <p className="section__paragraph">
               Beyond the screen, I find immense satisfaction in sharing my
               knowledge with the next generation. As a Code Club Tutor,
               I&lsquo;ve introduced children to the wonders of coding through
               Scratch, Python, and graphics, nurturing a love for technology
               from an early age.
             </p>
-            <p className="section-paragraph">
+            <p className="section__paragraph">
               Away from the keyboard, I embrace a balanced lifestyle, fueled by
               my passion for cycling, tai chi, and qigong. These practices not
               only enhance my well-being but also provide the mental clarity and
@@ -59,4 +59,4 @@ const AboutMe = () => {
   );
 };
 
-export default AboutMe;
+export default About;
