4a7645665f439d5f223b9434d38a00b6f0d34cf0
WarrenT <warrentyler1024@gmail.com>
2023-12-30 10:42:26 +0000

    Update App component structure and integrate new components
    - Add new components (About, HireMe, Skills, SideNav)
    - Implement state for theme switching (light/dark)
    - Integrate ScrollProvider for smooth scroll functionality
    - Include ScrollListener for handling scroll events
    - Update Header and Hero components to accept theme prop
    - Ensure consistency in theme application across components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
+import { useEffect, useState } from "react";
+import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import HireMe from "./components/HireMe";
 import Projects from "./components/Projects";
+import SideNav from "./components/SideNav";
+import Skills from "./components/Skills";
+import { ScrollProvider } from "./context/ScrollProvider";
+import ScrollListener from "./utilities/ScrollListener";
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    const userTheme = localStorage.getItem("theme");
+    const systemTheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const initialTheme = userTheme || (systemTheme ? "dark" : "light");
+    setTheme(initialTheme);
+  }, []);
+
   return (
-    <div className="dark">
+    <div className={theme === "dark" ? "dark" : ""}>
       {/* Main Container (Simulating Body) */}
       <div className="min-h-screen font-poppins pb-12 text-black dark:text-white bg-body dark:bg-body-dark">
-        <Header />
-        <Hero />
-        <Projects />
+        <ScrollProvider>
+          <Header theme={theme} toggleTheme={toggleTheme} />
+          <SideNav />
+          <Hero theme={theme} />
+          <About />
+          <Projects />
+          <Skills />
+          <HireMe />
+          <ScrollListener /> {/* Explicitly include it */}
+        </ScrollProvider>
       </div>
     </div>
   );
